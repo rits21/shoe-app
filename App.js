59ef5d50b104fe873795315fbe711663f91c0ea1
Rittika Majumder <119842177+rits21@users.noreply.github.com>
2024-01-04 01:11:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import Navigation from "./components/Navigation"
+import './App.css';
+import HeroSection from "./components/Hero";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <HeroSection/>
+    </div>
+  );
+}
+
+export default App;
